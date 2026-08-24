9438b5e1e0fb43352d46aefc8f869f96703a916d
mohammedflutterpro <ahmdesaber188@gmail.com>
2026-08-25 02:02:40 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import vinext from "vinext";
 import { defineConfig } from "vite";
+import vinext from "vinext";
+import rsc from "@vitejs/plugin-rsc";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
-export default defineConfig(async()=>{
- const {cloudflare}=await import("@cloudflare/vite-plugin");
- return {plugins:[vinext(),cloudflare({viteEnvironment:{name:"rsc",childEnvironments:["ssr"]},inspectorPort:false,config:{main:"./worker/index.ts",compatibility_flags:["nodejs_compat"]}})]};
+export default defineConfig({
+  plugins: [
+    vinext(),
+
+    rsc({
+      entries: {
+        rsc: "virtual:vinext-rsc-entry",
+        ssr: "virtual:vinext-app-ssr-entry",
+        client: "virtual:vinext-app-browser-entry",
+      },
+    }),
+
+    cloudflare({
+      viteEnvironment: {
+        name: "rsc",
+        childEnvironments: ["ssr"],
+      },
+      inspectorPort: false,
+      config: {
+        main: "./worker/index.ts",
+        compatibility_flags: ["nodejs_compat"],
+      },
+    }),
+  ],
 });
